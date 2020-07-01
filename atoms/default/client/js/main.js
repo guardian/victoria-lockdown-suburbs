1eb9fea5753cbc4c4b06ee0baf0c0be9cdfcc8ae
jerseywonder <work@andyball.info>
2020-07-01 10:54:02 +1000

    resiser

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -20,3 +20,33 @@ setTimeout(() => {
   window.resize()
   }
 },100)
+
+function onElementHeightChange(elm, callback) {
+    var lastHeight = elm.clientHeight, newHeight;
+    (function run(){
+        newHeight = elm.clientHeight;
+        if( lastHeight != newHeight )
+            callback();
+        lastHeight = newHeight;
+
+        if( elm.onElementHeightChangeTimer )
+            clearTimeout(elm.onElementHeightChangeTimer);
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 250);
+    })();
+}
+
+if (window.frameElement) {
+
+    // console.log("We are inside an iframe universe.")
+    // document.querySelector(".interactive-wrapper").style.overflow = "hidden";
+    // document.querySelector(".interactive-wrapper").style.borderTop = "solid 1px #dfdfdf";
+    // document.querySelector(".interactive-wrapper").style.borderBottom = "solid 1px #dfdfdf";
+    // document.querySelector(".interactive-wrapper").style.paddingTop = "2px";
+    // document.querySelector("#controls").style.maxWidth = "none";
+
+    onElementHeightChange(document.body, function() {
+        window.frameElement.height = document.body.offsetHeight + 150
+    });
+
+}
